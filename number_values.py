21464f6b4417b5476c629efe55b1c586f90d44de
Fahad Khan <khanfahad@gmail.com>
2019-06-12 21:45:05 -0500

    Create number_values.py
    Given an integer, prints the following values for each integer:
    
    Decimal
    Octal
    Hexadecimal (capitalized)
    Binary

---
+++ number_values.py
@@ -0,0 +1,8 @@
+x = int(input())
+w = len(str(bin(x)).replace('0b',''))
+for i in range(1, x+1):
+    b = bin(int(i)).replace('0b','').rjust(w, ' ')
+    o = oct(int(i)).replace('0o','', 1).rjust(w, ' ')
+    h = hex(int(i)).replace('0x','').upper().rjust(w, ' ')
+    j = str(i).rjust(w, ' ')
+    print(j, o, h, b)
